88bb7fb1850d3ee69d7d8dc0ed010474c0bce9f0
Jakub <gruchalski.kuba@gmail.com>
2021-08-16 16:51:09 +0200

    Save to db

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import sqlite3
 import praw
 
+from praw.models import Submission, Comment
 from database import create_connection
-from sql_func import create_tables
+from sql_func import create_tables, save_posts, save_comments
 from get_credentials import get_credentials
 
-def main():
+def init_reddit(credentials):
     creds = get_credentials()
-    conn = create_connection("test_db.db")
-    if conn is not None:
-        create_tables(conn)
-    else:
-        print('conn is None')
 
     reddit = praw.Reddit(
         client_id=creds['client_id'],
@@ -20,7 +16,31 @@ def main():
         username=creds['username'],
         password=creds['password']
     )
-    print(reddit.read_only)
+    return reddit
+
+def get_saved(reddit):
+    posts = []
+    comments = []
+    for item in reddit.user.me().saved(limit=None):
+        if isinstance(item, Submission):
+            posts.append(item)
+        elif isinstance(item, Comment):
+            comments.append(item)
+        else:
+            pass
+    return posts, comments
+
+def main():
+    creds = get_credentials()
+    conn = create_connection("reddit_saved.db")
+    if conn is not None:
+        create_tables(conn)
+    else:
+        print('Could not create database tables.')
+    
+    posts, comments = get_saved(init_reddit(get_credentials))
+    save_posts(conn, posts)
+    save_comments(conn, comments)
 
 if __name__ == '__main__':
     main()
--- sql_func.py
+++ sql_func.py
@@ -2,21 +2,44 @@ import sqlite3
 from sqlite3 import Error
 
 # TODO
-create_post_table = """ CREATE TABLE IF NOT EXISTS post (
+create_post_table = """CREATE TABLE IF NOT EXISTS post (
                         id integer PRIMARY KEY,
-                        post_id integer,
+                        post_id integer UNIQUE,
                         title text,
+                        fullname text,
+                        subreddit text,
                         url text,
                         permalink text,
-                        subreddit text,
-                        date text
+                        selftext text
                         ); """
 
-create_comment_table = """ CREATE TABLE IF NOT EXISTS comment (
+create_comment_table = """CREATE TABLE IF NOT EXISTS comment (
                             id integer PRIMARY KEY,
+                            comment_id integer UNIQUE,
                             body text,
+                            permalink text,
                             score integer
                             );"""
+
+
+insert_post = """INSERT INTO post (
+                        post_id,
+                        title,
+                        fullname,
+                        subreddit,
+                        url,
+                        permalink,
+                        selftext)
+                    VALUES (?, ?, ?, ?, ?, ?, ?);"""
+
+
+insert_comment = """INSERT INTO comment (
+                        comment_id,
+                        body,
+                        permalink,
+                        score)
+                    VALUES (?, ?, ?, ?);"""
+
 statements = [
     create_post_table,
     create_comment_table,
@@ -29,3 +52,36 @@ def create_tables(conn):
             c.execute(s)
     except Error as e:
         print(e)
+
+def save_posts(conn, posts):
+    c = conn.cursor()
+    for post in posts:
+        try:
+            values = (
+                post.id,
+                post.title,
+                post.fullname,
+                post.subreddit.display_name,
+                post.url,
+                post.permalink,
+                post.selftext
+            )
+            c.execute(insert_post, values)
+        except Error:
+            c.execute("SELECT count(*) FROM post WHERE post_id = ?", (values[0], ))
+    conn.commit()
+
+def save_comments(conn, comments):
+    c = conn.cursor()
+    for comm in comments:
+        try:
+            values = (
+                comm.id,
+                comm.body,
+                comm.permalink,
+                comm.score
+            )
+            c.execute(insert_comment, values)
+        except Error:
+            c.execute("SELECT count(*) FROM post WHERE post_id = ?", (values[0], ))
+    conn.commit()
