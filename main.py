cfcd61f676a05fae8dfff3710d06787fa51a3992
Jakub <gruchalski.kuba@gmail.com>
2021-08-27 18:40:16 +0200

    Revert "Add first argument"
    This reverts commit d15231f539e2cee5a7076fbd5d15e64be7bff7f7.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import praw
 import json
-import argparse
 
 from getpass import getpass
 from database import Db
@@ -8,21 +7,9 @@ from user import User
 from authorize import Authorize
 
 def main():
-    parser = argparse.ArgumentParser(description="Saves posts and comments from user's reddit account.")
-    parser.add_argument('-c', '--credentials', dest='credentials', required=False)
-    args = parser.parse_args()
-
-    user_credentials = {}
-
-    if args.credentials:
-        with open(args.credentials, 'r') as f:
-            data = json.load(f)
-            user_credentials = {**data}
-
-
-    db = Db('test.db')
+    db = Db('reddit_saved.db')
     auth = Authorize()
-    # user_credentials = auth.get_user_credentials()
+    user_credentials = auth.get_user_credentials()
     user = User(**user_credentials)
     posts, comments = user.get_saved(user.login())
 
