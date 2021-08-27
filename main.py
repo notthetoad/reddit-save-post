d15231f539e2cee5a7076fbd5d15e64be7bff7f7
Jakub <gruchalski.kuba@gmail.com>
2021-08-27 18:39:01 +0200

    Add first argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import json
+import argparse
 
 from getpass import getpass
 from database import Db
@@ -7,9 +8,21 @@ from user import User
 from authorize import Authorize
 
 def main():
-    db = Db('reddit_saved.db')
+    parser = argparse.ArgumentParser(description="Saves posts and comments from user's reddit account.")
+    parser.add_argument('-c', '--credentials', dest='credentials', required=False)
+    args = parser.parse_args()
+
+    user_credentials = {}
+
+    if args.credentials:
+        with open(args.credentials, 'r') as f:
+            data = json.load(f)
+            user_credentials = {**data}
+
+
+    db = Db('test.db')
     auth = Authorize()
-    user_credentials = auth.get_user_credentials()
+    # user_credentials = auth.get_user_credentials()
     user = User(**user_credentials)
     posts, comments = user.get_saved(user.login())
 
