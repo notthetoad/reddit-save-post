972093782b95fbe01b4b11d4c7b4c5de75f62d7b
Jakub <gruchalski.kuba@gmail.com>
2021-08-27 16:23:30 +0200

    Move get_saved() function to User class as method

--- authorize.py
+++ authorize.py
@@ -7,16 +7,6 @@ class Authorize:
     def __init__(self):
         self.get_user_credentials()
 
-    def check_for_credentials(self):
-        credentials_file = os.path.isfile('./credentials.json')
-        if credentials_file:
-            with open('credentials.json', 'r') as f:
-                data = json.load(f)
-                credentials_from_file = {**data}
-        else:
-            pass
-        return credentials_from_file
-
     def get_user_credentials(self):
         user_credentials = self.check_for_credentials()
         if len(user_credentials.items()) > 4:
@@ -34,3 +24,14 @@ class Authorize:
         else:
             pass
         return user_credentials
+
+    def check_for_credentials(self):
+        credentials_from_file = {}
+        file_path = os.path.isfile('./credentials.json')
+        if file_path:
+            with open('credentials.json', 'r') as f:
+                data = json.load(f)
+                credentials_from_file = {**data}
+        else:
+            pass
+        return credentials_from_file
--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 import praw
 import json
 
-from praw.models import Submission, Comment
 from getpass import getpass
 from database import Db
 from user import User
 from authorize import Authorize
-
-def get_saved(reddit):
-    posts = []
-    comments = []
-
-    for item in reddit.user.me().saved(limit=None):
-        if isinstance(item, Submission):
-            posts.insert(0, item)
-        elif isinstance(item, Comment):
-            comments.insert(0, item)
-        else:
-            pass
-    return posts, comments
 
 def main():
     db = Db('reddit_saved.db')
     auth = Authorize()
     user_credentials = auth.get_user_credentials()
     user = User(**user_credentials)
-    posts, comments = get_saved(user.login())
+    posts, comments = user.get_saved(user.login())
 
     db.save_posts(posts)
     db.save_comments(comments)
