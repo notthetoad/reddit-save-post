713605796e39cbbac82f2d56fbd3499f69c4b649
Jakub <gruchalski.kuba@gmail.com>
2022-02-21 10:49:21 +0100

    First refactor. Cannot test no working internet connection

--- user.py
+++ user.py
@@ -6,31 +6,34 @@ from praw.models import Submission, Comment
 
 class User:
 
-    def __init__(self, client_id, client_secret, username, password, user_agent):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.username = username
-        self.password = password
-        self.user_agent = username
+    def __init__(self, file):
+        self.file = file
+        self.user_data = self.authorize()
+        self.r_instance = self.login()
+
+    def authorize(self):
+        with open(self.file) as f:
+            data = json.load(f)
+            return {**data}
 
     def login(self):
         try:
             reddit = praw.Reddit(
-            client_id=self.client_id,
-            client_secret=self.client_secret,
-            username=self.username,
-            password=self.password,
-            user_agent=self.user_agent
+                client_id=self.user_data['client_id'],
+                client_secret=self.user_data['client_secret'],
+                username=self.user_data['username'],
+                password=self.user_data['password'],
+                user_agent=self.user_data['user_agent']
             )
-        except RedditAPIException as e:
-            print(e)
+        except RedditAPIException as error:
+            print(error)
         return reddit
 
-    def get_saved(self, reddit_instance):
+    def get_saved(self):
         posts = []
         comments = []
 
-        for item in reddit_instance.user.me().saved(limit=None):
+        for item in self.r_instance.user.me().saved(limit=None):
             if isinstance(item, Submission):
                 posts.insert(0, item)
             elif isinstance(item, Comment):
