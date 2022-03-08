771325b017ae149446d82ae2a679879f1115c98a
Jakub <gruchalski.kuba@gmail.com>
2022-03-08 19:21:02 +0100

    Add save_to_file for UserCredentials

--- authorize.py
+++ authorize.py
@@ -14,6 +14,11 @@ class UserCredentials:
         self.user_info['client_id'] = input('client_id: ')
         self.user_info['client_secret'] = input('client_secret: ')
         self.user_info['user_agent'] = input('user_agent: ')
+        save = input("Do you wish to save credentials to file? [y/N]: ")
+        if save == 'y':
+            name = input("Please enter file name: ")
+            if name != None:
+                self.save_credentials_to_file(name)
         return self.user_info
 
     def get_user_cred_from_file(self, file):
@@ -28,3 +33,10 @@ class UserCredentials:
         else:
             self.get_user_credentials()
         return self.user_info
+
+    def save_credentials_to_file(self, f_name):
+        with open(f"{f_name}.json", 'w') as fp:
+            json.dump(self.user_info, fp)
+        print("saved to file")
+
+
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from authorize import UserCredentials
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Saves posts and comments from user's reddit account.")
-    parser.add_argument('-c', '--credentials', dest='credentials', metavar='[file]', required=False, help="file containing user credentials")
+    #parser.add_argument('-c', '--credentials', dest='credentials', metavar='[file]', required=False, help="file containing user credentials")
+    parser.add_argument('-f', '--file', dest='file', nargs='?', type=argparse.FileType('r'), help="path to file foo") 
     parser.add_argument('-d', '--database', dest='database', metavar='[name]', required=False, help="choose a name for your database file")
     parser.add_argument('--export', dest='export', required=False, action='store_true', help="export database to Comma Separated Values (CSV) file")
     parser.add_argument('--format', dest='format', required=False, action='store_true', help="show format for credentials file")
@@ -18,13 +19,19 @@ def main():
 
     db_name = 'test.db'
     db = Db(db_name)
-    if parsed_args.credentials:
-        user = User(parsed_args.credentials)
+#    if parsed_args.credentials:
+#        user = User(parsed_args.credentials)
+#    else:
+#        user = UserCredentials().get_credentials()
+#    posts, comments = user.get_saved()
+#    db.save_posts(posts)
+#    db.save_comments(comments)
+    #TODO doesn't want to go to else if arg not given, maybe empty list?
+    if parsed_args.file:
+        user = User(parsed_args.file)
+        print(user)
     else:
-        user = UserCredentials().get_credentials()
-    posts, comments = user.get_saved()
-    db.save_posts(posts)
-    db.save_comments(comments)
+        UserCredentials().get_credentials()
 
     if parsed_args.format:
         sys.stdout.write('{\n\t"username": username,\n\t"password": password,\n\t"client_id": client_id,\n\t"client_secret": client_secret\n\t"user_agent": user_agent\n}\n')
